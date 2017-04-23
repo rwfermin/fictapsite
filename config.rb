9f2eb683b7b8e47db76c14096fcdf0376115b690
Viel Russel Fermin <rwfermin@gmail.com>
2017-04-23 16:27:33 +0800

    New section / enabled tech session on gallery / modifications on home and news subpages

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'static/css'
+sass_dir = 'static/sass'
+images_dir = 'static/images'
+javascripts_dir = 'static/scripts'
+relative_assets = true
+line_comments = true
+output_style = :compressed
